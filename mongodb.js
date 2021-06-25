2420e821676452576322d85b261287f6d64602b8
Quang Huy <81632425+huybq26@users.noreply.github.com>
2021-06-25 11:28:19 +0800

    Add files via upload

---
+++ mongodb.js
@@ -0,0 +1,45 @@
+// CRUD create read update delete
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const ObjectID = mongodb.ObjectID;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(
+  connectionURL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (error, client) => {
+    if (error) {
+      return console.log('Unable to connect to database!');
+    }
+
+    const db = client.db(databaseName);
+
+    // db.collection('users')
+    //   .deleteMany({
+    //     age: 20,
+    //   })
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    db.collection('users')
+      .deleteOne({
+        completed: true,
+      })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+);
